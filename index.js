e6617662032ff25b80f5b4c85c31af9f5812977e
DN <dnkaplun1987@gmail.com>
2021-10-15 07:22:34 +0300

    151020210723

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3500;
-const server = require('http').createServer(app);
+const path = require('path');
+const server = require('http').Server(app).listen(443);
 const io = require('socket.io')(server);
+const port = process.env.PORT || 3500;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -12,13 +13,26 @@ app.use(cors());
 app.options('*', cors());
 
 const routes = require('./settings/routes');
-const chatHistory = [];
+const chatHistory = [
+    {
+        message: 'message',
+        userName: 'User 1'
+    },
+    {
+        message: 'message',
+        userName: 'User 2'
+    },
+    {
+        message: 'message',
+        userName: 'User 3'
+    }
+];
 routes(app);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', function (req, res) {
+    app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 };
@@ -28,44 +42,17 @@ app.listen(port, () => {
 });
 
 io.on('connection', (socket) => {
+    console.log(`WS start`)
 
-    socket.emit('chat history', chatHistory);
+    socket.emit('chatHistory', chatHistory);
 
-    socket.on('new user enter', (userName) => {
-        socket.broadcast.emit('new user enter', {
-          username: userName
-        });
-        socket.username = userName;
-    });
-
-    socket.on('typing', () => {
-        socket.broadcast.emit('typing', {
-          username: socket.username
-        });
-    });
-    
-    socket.on('stop typing', () => {
-        socket.broadcast.emit('stop typing', {
-          username: socket.username
-        });
-    });
-
-    socket.on('new message', (data) => {
-        socket.broadcast.emit('new message', {
-          username: socket.username,
-          message: data
-        });
-        chatHistory.push({
-            username: socket.username,
-            message: data
-          })
+    socket.on('newMessage', (data) => {
+        socket.broadcast.emit('newMessage', data);
+        chatHistory.push(data)
     });
 
     socket.on('disconnect', (data) => {
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data
-          });
+        console.log(`WS stop`)
     });
 
 });
