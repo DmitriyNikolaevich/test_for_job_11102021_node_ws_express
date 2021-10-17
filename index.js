041ace57ba1cf7823d1ddeb7dbf177907ed41fd6
DN <dnkaplun1987@gmail.com>
2021-10-17 19:33:38 +0300

    deploy 6

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 app.options('*', cors());
 
 const routes = require('./settings/routes');
