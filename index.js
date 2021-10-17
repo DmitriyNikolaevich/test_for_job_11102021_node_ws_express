fc0b44d5364e91ecc8984c43767378183852fcf1
DN <dnkaplun1987@gmail.com>
2021-10-17 21:25:58 +0300

    deploy

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ routes(app);
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'build')));
 
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+    // app.get('*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // });
 };
 
 app.listen(port, () => {
