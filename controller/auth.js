f24c92704374b7c11f21ccdbefd73b4dee45b845
DN <dnkaplun1987@gmail.com>
2021-10-17 18:50:04 +0300

    deploy 3

--- controller/auth.js
+++ controller/auth.js
@@ -40,6 +40,4 @@ exports.auth = (req, res) => {
 
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Credentials', true)
 }
